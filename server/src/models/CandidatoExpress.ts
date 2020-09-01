645558904c1ef3ac2e25e02888a9a2a8171d4d88
DiovanePacheti <frontendestudo@gmail.com>
2020-09-01 03:36:32 -0300

    criando a class CandidatoExpress

---
+++ server/src/models/CandidatoExpress.ts
@@ -0,0 +1,55 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { uuid } from 'uuidv4';
+
+@Entity('candidato_express')
+export default class CandidatoExpress {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    telefone: string;
+
+    @Column()
+    curso: string;
+
+    @Column({
+        type: 'date',
+        nullable: true
+      })
+    previsaoFormatura:Date;
+
+    @Column()
+    valorContratado:number;
+
+    @Column()
+    parcelasCobertas:number;
+
+    @Column()
+    condicaoDaRestituicao:number;
+
+    @Column({ nullable: false, type: 'simple-json'  })
+    validade:Date[];
+
+    @Column({
+        type: 'date',
+        nullable: true
+      })
+    inicioDaRestituicao:Date;
+
+    @Column()
+    convenio:string;
+
+    @Column()
+    solucao:string;
+
+}
